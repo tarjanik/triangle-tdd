182bde0ccf5fe817e41f6f7af318a7e8b9c428d6
Kristóf <tarjanik@gmail.com>
2018-05-12 14:47:50 +0200

    Initial release

---
+++ src/TriangleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void horizontalLineTest(){
+        // #Scenario 1_dist.jpg [1]
+        // Draw a line from (0,0) to (5,0)
+        // Verify: length of the line is 5
+    }
+}
